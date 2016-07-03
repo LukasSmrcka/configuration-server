6c5883c1fcfdbf287059dd2398940e2b7bde4ae5
LukasSmrcka <lukas.smrcka@gmail.com>
2016-07-03 19:47:36 +0200

    Initial

---
+++ src/test/java/com/luk3c/ConfigurationServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.luk3c;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = ConfigurationServerApplication.class)
+public class ConfigurationServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
